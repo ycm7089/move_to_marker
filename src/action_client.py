a3ac6df869cf0213acc0b02aff03daa6285acd0c
ycm7089 <ycm7089>
2022-07-22 14:50:39 +0900

    s

--- src/action_client.py
+++ src/action_client.py
@@ -18,16 +18,20 @@ def fibonacci_client():
     # Waits until the action server has started up and started
     # listening for goals.
     client.wait_for_server()
-
+    cm_list = list(range(2, 11))
+    print(cm_list)
     # Creates a goal to send to the action server.
-    goal = actionlib_tutorials.msg.FibonacciGoal(order=10)
+    for i in cm_list:
+        goal = actionlib_tutorials.msg.FibonacciGoal(order=i)
 
-    # Sends the goal to the action server.
-    client.send_goal(goal)
+        # Sends the goal to the action server.
+        client.send_goal(goal)
 
-    # Waits for the server to finish performing the action.
-    client.wait_for_result()
-
+        # Waits for the server to finish performing the action.
+        client.wait_for_result()
+        print(goal)
+        print(client.get_result())
+            
     # Prints out the result of executing the action
     return client.get_result()  # A FibonacciResult
 
