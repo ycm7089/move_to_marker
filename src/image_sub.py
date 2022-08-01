4d31292e5b9289b55099025be2d82b04a0928237
CM_Youn <ycm7089@naver.com>
2022-08-01 11:28:38 +0900

    marker_pose

--- src/image_sub.py
+++ src/image_sub.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+from termios import tcdrain
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
@@ -13,6 +14,13 @@ class CameraImage():
 
         self.rgb_image = Image()
         self.cv_br = CvBridge()
+        
+        self.k = np.array(
+            [[1245.133331, 0.0, 1021.56411],
+             [0.0, 1244.702243, 765.638118],
+             [0.0, 0.0, 1.0]])
+
+        self.d = np.array([-0.355604, 0.108672, -0.000292, 0.000296, 0.0])
 
         # Subscribers
         self.image_sub = rospy.Subscriber("/camera/image_raw", Image, self.camera_cb)
@@ -25,16 +33,32 @@ class CameraImage():
         cv2.imshow('test', self.rgb_image)
         cv2.waitKey(1)
 
-    def findArucoMarkers(self, img, markerSize = 5, totalMarkers=250, draw=True):
+    def findArucoMarkers(self, img, draw=True):
+
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        key = getattr(aruco, f'DICT_{markerSize}X{markerSize}_{totalMarkers}')
+        key = getattr(aruco, f'DICT_ARUCO_ORIGINAL')
+
         arucoDict = aruco.Dictionary_get(key)
         arucoParam = aruco.DetectorParameters_create()
-        bboxs, ids, rejected = aruco.detectMarkers(gray, arucoDict, parameters = arucoParam)
-        print(ids)
+        
+        bboxs, ids, rejected = cv2.aruco.detectMarkers(gray, arucoDict, parameters = arucoParam, 
+                                                       cameraMatrix=self.k, distCoeff=self.d)
 
-        if draw:
-            aruco.drawDetectedMarkers(img, bboxs)
+        # If markers are detected
+        if len(bboxs) > 0:
+            for i in range(0, len(ids)):
+                # Estimate pose of each marker and return the values rvec and tvec---(different from those of camera coefficients)
+                # 0.1 : Aruco-Marker size, k : camera K, d : camera D
+                rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(bboxs[i], 0.1, self.k,
+                                                                        self.d)
+                # Draw a square around the markers
+                cv2.aruco.drawDetectedMarkers(img, bboxs) 
+
+                # Draw Axis
+                cv2.aruco.drawAxis(img, self.k, self.d, rvec, tvec, 0.05)  
+                print(tvec)
+
+        return img
 
 if __name__ == '__main__':
     try:
